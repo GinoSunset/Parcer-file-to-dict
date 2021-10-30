92a3df3fc208f378d241fcd67643e182dc0418cb
GinoSunset <ginosunset@yandex.ru>
2021-10-30 22:13:27 +0300

    refact: extract methods to defined line

--- parser.py
+++ parser.py
@@ -8,6 +8,7 @@ key_value_string_re = re.compile(r"^\S.+:.*")
 
 @contextmanager
 def open_parse_file(path: str) -> TextIOWrapper:
+    file = None
     try:
         file = open(path, "r")
         yield file
@@ -19,26 +20,44 @@ def open_parse_file(path: str) -> TextIOWrapper:
 def parse_file(path):
     documents = []
     current_docs = {}
-    key = ""
+    current_key = ""
     with open_parse_file(path) as file:
         for line in file:
-            if line.startswith("#"):
+            if is_comment_line(line):
                 continue
-            if line == "\n" or line == "\r\n":
+            if is_separating_line(line):
                 if current_docs != {}:
                     documents.append(current_docs)
                     current_docs = {}
                 continue
-            if re.match(key_value_string_re, line):
-                key, value = line.split(":", 1)
-                value = value.lstrip().rstrip("\n")
-                if key in current_docs:
-                    current_docs[key] += "\n" + value
-                    continue
-                current_docs[key] = value
-                continue
-            current_docs[key] += "\n" + line.lstrip().rstrip("\n")
+            if is_line_with_key_value(line):
+                current_key, line = line.split(":", 1)
+            value = cleaned_value(line)
+            update_current_docs(current_docs, current_key, value)
     return documents
+
+
+def update_current_docs(current_docs: dict, key: str, value: str):
+    if key in current_docs:
+        current_docs[key] += "\n" + value
+        return
+    current_docs[key] = value
+
+
+def cleaned_value(value: str):
+    return value.lstrip().rstrip("\n")
+
+
+def is_line_with_key_value(line: str):
+    return re.match(key_value_string_re, line)
+
+
+def is_separating_line(line: str):
+    return line == "\n" or line == "\r\n"
+
+
+def is_comment_line(line: str):
+    return line.startswith("#")
 
 
 def load_data(path):
